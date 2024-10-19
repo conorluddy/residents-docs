205a7f3c733ba00df888292abdb4d40e92122df5
Conor Luddy <conorluddy@gmail.com>
2024-10-19 11:55:18 +0100

    Initial documentation structure

--- settings/documents.ts
+++ settings/documents.ts
@@ -7,87 +7,104 @@ export const Documents: Paths[] = [
     heading: "Getting started",
     items: [
       {
+        title: "Overview",
+        href: "/overview",
+      },
+      {
+        title: "Features",
+        href: "/features",
+      },
+      {
+        title: "Stack & Dependencies",
+        href: "/stack-dependencies-prerequisites",
+      },
+      {
         title: "Installation",
         href: "/installation",
       },
+    ],
+  },
+  {
+    spacer: true,
+  },
+  {
+    title: "Database",
+    href: "/schema",
+    items: [
       {
-        title: "Setup",
-        href: "/setup",
+        title: "Schema",
+        href: "/schema",
       },
       {
-        title: "Changelog",
-        href: "/changelog",
+        title: "ORM",
+        href: "/swapping-databases-or-orm",
+      },
+      {
+        title: "System",
+        href: "/relational-db",
       },
     ],
   },
-  // {
-  //   spacer: true,
-  // },
-  // {
-  //   title: "Navigation",
-  //   href: "/navigation",
-  //   heading: "Documents",
-  // },
-  // {
-  //   title: "Structure",
-  //   href: "/structure",
-  //   items: [
-  //     {
-  //       title: "Deeper",
-  //       href: "/deeper",
-  //       items : [
-  //         {
-  //           title: "Even deeper",
-  //           href: "/even-deeper",
-  //         },
-  //       ]
-  //     },
-  //   ],
-  // },
-  // {
-  //   spacer: true,
-  // },
-  // {
-  //   title: "Markup",
-  //   href: "/markup",
-  //   heading: "Components",
-  //   items: [
-  //     {
-  //       title: "Cards",
-  //       href: "/cards",
-  //     },
-  //     {
-  //       title: "Diagrams",
-  //       href: "/diagrams",
-  //     },
-  //     {
-  //       title: "Filetree",
-  //       href: "/filetree",
-  //     },
-  //     {
-  //       title: "Lists",
-  //       href: "/lists",
-  //     },
-  //     {
-  //       title: "Maths",
-  //       href: "/maths",
-  //     },
-  //     {
-  //       title: "Notes",
-  //       href: "/notes",
-  //     },
-  //     {
-  //       title: "Steps",
-  //       href: "/steps",
-  //     },
-  //     {
-  //       title: "Table",
-  //       href: "/table",
-  //     },
-  //     {
-  //       title: "Tabs",
-  //       href: "/tabs",
-  //     },
-  //   ],
-  // },
+  {
+    title: "Development",
+    href: "/development",
+    items: [
+      {
+        title: "Local",
+        href: "/local",
+      },
+      {
+        title: "Docker",
+        href: "/docker",
+      },
+      {
+        title: "Tooling",
+        href: "/tooling",
+      },
+      {
+        title: "Testing",
+        href: "/testing",
+      },
+    ],
+  },
+  {
+    title: "Authentication & Authorization",
+    href: "/rbac-authorisation-authentication",
+  },
+  {
+    title: "Deployment",
+    href: "/deployment",
+  },
+  {
+    title: "Examples",
+    href: "/examples",
+  },
+  {
+    title: "Extending",
+    href: "/extending",
+  },
+  {
+    title: "Frontend",
+    href: "/front-end-components-and-examples",
+  },
+  {
+    title: "Customization",
+    href: "/customisation",
+  },
+  {
+    title: "Troubleshooting",
+    href: "/troubleshooting",
+  },
+  {
+    title: "FAQ",
+    href: "/frequently-asked-questions",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+  {
+    title: "Terms & Conditions",
+    href: "/terms-and-conditions",
+  },
 ];
